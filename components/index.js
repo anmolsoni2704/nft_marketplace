4b6a3155c0d9171ab2d93c9191ec5fa34e512000
unknown <anmolsoni2704.com>
2022-12-22 20:11:06 +0530

    create-nft page added

--- components/index.js
+++ components/index.js
@@ -5,5 +5,6 @@ import Button from './Button';
 import Banner from './Banner';
 import NFTCard from './NFTCard';
 import CreatorCard from './CreatorCard';
+import Input from './Input';
 
-export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard };
+export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard, Input };
